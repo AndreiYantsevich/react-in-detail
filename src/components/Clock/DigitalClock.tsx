7d3fa49ff554c925456661eea47878fda6920099
AndreiYantsevich <yantsevich92@gmail.com>
2021-10-31 23:01:59 +0300

    add clearInterval to DigitalClock

---
+++ src/components/Clock/DigitalClock.tsx
@@ -0,0 +1,26 @@
+import React, {useEffect, useState} from 'react';
+
+type PropsType = {}
+
+const DigitalClock: React.FC<PropsType> = (props) => {
+
+    const [state, setState] = useState(new Date())
+
+    useEffect(() => {
+        const intervalID = setInterval(() => {
+            setState(new Date());
+        }, 1000);
+
+        return () => {
+            clearInterval(intervalID)
+        }
+    }, [])
+
+    return (
+        <div>
+            <h1>{state.toLocaleTimeString()}</h1>
+        </div>
+    );
+};
+
+export default DigitalClock;
